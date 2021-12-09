f6e18e46ed37b03f3032156b6129f01e3aa72d99
JasonLuis <jason20luis@gmail.com>
2021-12-09 14:20:23 -0300

    chore: vue with sass vite and typescript

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="main">
+    <div class="main__card"></div>
+    <div class="main__card--active"></div>
+    <div class="main__card--inactive"></div>
+  </div>
+
+  <p>
+    Recommended IDE setup:
+    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+    +
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+  </p>
+
+  <p>See <code>README.md</code> for more information.</p>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Docs
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+  </p>
+
+  <button type="button" @click="count++">count is: {{ count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<style lang="scss" scoped>
+// CSS Bem
+// https://desenvolvimentoparaweb.com/css/bem/
+
+@mixin theme($color: DarkGray) {
+  background-color: $color;
+    width: 150px;
+    height: 150px;
+}
+
+.main {
+  display: flex;
+  justify-content: center;
+
+  &__card {
+     @include theme($color: DarkRed);
+     &--active {
+       @include theme($color: DarkGreen);
+     }
+     &--inactive {
+       @include theme($color: DarkGray);
+     }
+  }
+}
+
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
